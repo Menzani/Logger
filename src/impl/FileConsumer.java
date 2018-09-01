f610a68acfc03626ac9e664292e01f5f746e8f86
Francesco Menzani <francesco@menzani.eu>
2018-09-01 15:52:36 +0200

    Fix FileConsumer concurrency issue

--- src/impl/FileConsumer.java
+++ src/impl/FileConsumer.java
@@ -5,17 +5,14 @@ import it.menzani.logger.Lazy;
 import it.menzani.logger.api.Consumer;
 
 import java.io.IOException;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
+import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
-public final class FileConsumer implements Consumer, Lazy.Initializer<Writer> {
-    private static final String lineSeparator = System.lineSeparator();
-
+public final class FileConsumer implements Consumer, Lazy.Initializer<PrintWriter> {
     private final Path file;
-    private final Lazy<Writer> writer = new AtomicLazy<>(this, 10);
+    private final Lazy<PrintWriter> writer = new AtomicLazy<>(this, 10);
 
     public FileConsumer(Path file) {
         this.file = file;
@@ -23,14 +20,12 @@ public final class FileConsumer implements Consumer, Lazy.Initializer<Writer> {
 
     @Override
     public void consume(LogEntry entry, String formattedEntry) throws Exception {
-        Writer writer = this.writer.get();
-        writer.write(formattedEntry);
-        writer.write(lineSeparator);
-        writer.flush();
+        PrintWriter writer = this.writer.get();
+        writer.println(formattedEntry);
     }
 
     @Override
-    public Writer newInstance() throws IOException {
-        return new OutputStreamWriter(Files.newOutputStream(file, StandardOpenOption.CREATE, StandardOpenOption.APPEND));
+    public PrintWriter newInstance() throws IOException {
+        return new PrintWriter(Files.newOutputStream(file, StandardOpenOption.CREATE, StandardOpenOption.APPEND), true);
     }
 }

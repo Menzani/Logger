13a5f16036b979c6199d8f118f10a5bdc874d248
Francesco Menzani <francesco@menzani.eu>
2018-10-09 00:54:46 +0200

    Improve line separator normalization performance

--- src/impl/MessageFormatter.java
+++ src/impl/MessageFormatter.java
@@ -2,12 +2,16 @@ package it.menzani.logger.impl;
 
 import it.menzani.logger.api.Formatter;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class MessageFormatter implements Formatter {
+    private static final Pattern markers = Pattern.compile("%n|\\r?\\n");
     private static final String lineSeparator = System.lineSeparator();
 
     @Override
     public String format(LogEntry entry) throws EvaluationException {
-        String message = entry.getMessage().toString();
-        return message.replaceAll("%n|\\r?\\n", lineSeparator);
+        Matcher matcher = markers.matcher(entry.getMessage().toString());
+        return matcher.replaceAll(lineSeparator);
     }
 }

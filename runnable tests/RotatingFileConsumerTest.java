5e65055c8f1ed6e049b3c14a7890da858d12a16b
Francesco Menzani <francesco@menzani.eu>
2018-10-31 01:01:42 +0100

    License test source code under Mozilla Public License 2.0

--- runnable tests/RotatingFileConsumerTest.java
+++ runnable tests/RotatingFileConsumerTest.java
@@ -1,3 +1,9 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 import it.menzani.logger.api.Logger;
 import it.menzani.logger.api.RotationPolicy;
 import it.menzani.logger.impl.*;
